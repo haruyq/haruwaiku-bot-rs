ae4a2bd293f880a4191e4d1ea897a0bfee79dc07
haruyq <haruyqhaisinyou@gmail.com>
2025-10-14 16:04:54 +0900

    Add Url validation to xlinkconvert command

--- src/commands/xlinkconvert.rs
+++ src/commands/xlinkconvert.rs
@@ -1,4 +1,5 @@
 use crate::{Context, Error};
+use url::{Url};
 
 #[poise::command(slash_command, guild_only)]
 pub async fn xlinkconvert(
@@ -6,7 +7,17 @@ pub async fn xlinkconvert(
     #[description = "変換するURL"]
     url: String,
 ) -> Result<(), Error> {
-    
+    let uri: Url = match Url::parse(&url) {
+        Ok(u) => u,
+        Err(_) => {
+            ctx.say("無効なURLです。").await?;
+            return Ok(());
+        }
+    };
+    if uri.host().map_or(true, |h| h != url::Host::Domain("x.com")) {
+        ctx.say("URLはX.comのものでなければなりません。").await?;
+        return Ok(());
+    }
     let converted_url = url.replace("https://x.com/", "https://fxtwitter.com/");
     ctx.say(converted_url).await?;
     Ok(())
